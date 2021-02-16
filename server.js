ca321d0441e099ae88004a17defd0514c8755f66
Jones <Quincyj92@yahoo.com>
2021-02-15 20:35:36 -0600

    created employee members

---
+++ server.js
@@ -0,0 +1,55 @@
+const employees = [
+    {
+        name: 'Jeremy Bivens',
+        role: 'Cashier',
+        id: '0123456',
+        email: 'JerBiv012@homerwork10.com'
+        Document.getElementById('0123456').id,
+        Document.getElementByName('Jeremy Bivens').name,
+        Document.getElementByRole('Cashier').role&mdash;returns 'Employee',
+        Document.getElementByEmail('JerBiv012@homework10.com').email,
+
+
+    },
+    {
+        name: 'DaVish James',
+        role: 'Cashier',
+        id: '1234567',
+        email: 'DaVJam123@homerwork10.com'
+        Document.getElementById('1234567').id,
+        Document.getElementByName('DaVish James').name,
+        Document.getElementByRole('Cashier').role&mdash;returns 'Employee',
+        Document.getElementByEmail('DaVJam123@homerwork10.com').email,
+ 
+    }
+    {
+        name: 'Karlene Deans',
+        role: 'Greeter',
+        id: '2345678',
+        email: 'KarDea234@homerwork10.com'
+        Document.getElementById('2345678').id,
+        Document.getElementByName('Karlene Deans').name,
+        Document.getElementByRole('Greeter').role&mdash;returns 'Employee',
+        Document.getElementByEmail('KarDea234@homerwork10.com').email,
+    }
+    {
+        name: 'Jared Problems',
+        role: 'Custodian',
+        id: '3456789',
+        email: 'JarPro345@homerwork10.com'
+        Document.getElementById('3456789').id,
+        Document.getElementByName('Jared Problems').name,
+        Document.getElementByRole('Custidian').role&mdash;returns 'Employee',
+        Document.getElementByEmail('JarPro345@homerwork10.com').email,
+    }
+    {
+        name: 'Viktor Snead',
+        role: 'Stocker',
+        id: '45678980',
+        email: 'VikSne456@homerwork10.com'
+        Document.getElementById('4567890').id,
+        Document.getElementByName('Viktor Snead ').name,
+        Document.getElementByRole('Stocker').role&mdash;returns 'Employee',
+        Document.getElementByEmail('VikSne456@homerwork10.com').email,
+    }
+]
