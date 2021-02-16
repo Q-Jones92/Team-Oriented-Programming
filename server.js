9abd8fd6e9d02cda5b765e3b3498fe2beb735626
Jones <Quincyj92@yahoo.com>
2021-02-15 21:28:30 -0600

    created engineer members

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const employees = [
         email: 'JerBiv012@homerwork10.com'
         Document.getElementById('0123456').id,
         Document.getElementByName('Jeremy Bivens').name,
-        Document.getElementByRole('Cashier').role&mdash;returns 'Employee',
+        Document.getElementByRole('Cashier').role;returns 'Employee',
         Document.getElementByEmail('JerBiv012@homework10.com').email,
 
 
@@ -18,7 +18,7 @@ const employees = [
         email: 'DaVJam123@homerwork10.com'
         Document.getElementById('1234567').id,
         Document.getElementByName('DaVish James').name,
-        Document.getElementByRole('Cashier').role&mdash;returns 'Employee',
+        Document.getElementByRole('Cashier').role;returns 'Employee',
         Document.getElementByEmail('DaVJam123@homerwork10.com').email,
  
     }
@@ -29,7 +29,7 @@ const employees = [
         email: 'KarDea234@homerwork10.com'
         Document.getElementById('2345678').id,
         Document.getElementByName('Karlene Deans').name,
-        Document.getElementByRole('Greeter').role&mdash;returns 'Employee',
+        Document.getElementByRole('Greeter').role;returns 'Employee',
         Document.getElementByEmail('KarDea234@homerwork10.com').email,
     }
     {
@@ -49,7 +49,7 @@ const employees = [
         email: 'VikSne456@homerwork10.com'
         Document.getElementById('4567890').id,
         Document.getElementByName('Viktor Snead ').name,
-        Document.getElementByRole('Stocker').role&mdash;returns 'Employee',
+        Document.getElementByRole('Stocker').role;returns 'Employee',
         Document.getElementByEmail('VikSne456@homerwork10.com').email,
     }
 ]
@@ -63,7 +63,7 @@ Const managers = [
         officenumber: 'A12'
         Document.getElementById('1111111').id,
         Document.getElementByName('Anakin Skywalker').name,
-        Document.getElementByRole('Store Manager').role&mdash;overridden to return 'Manager',
+        Document.getElementByRole('Store Manager').role;overridden to return 'Manager',
         Document.getElementByEmail('AnaSky000@homework10.com').email,
     }
     {
@@ -74,18 +74,45 @@ Const managers = [
         officenumber: 'B12'
         Document.getElementById('0000000').id,
         Document.getElementByName('Benjamin Solo').name,
-        Document.getElementByRole('Production Manager').role&mdash;overridden to return 'Manager',
+        Document.getElementByRole('Production Manager').role;overridden to return 'Manager',
         Document.getElementByEmail('BenSol000@homework10.com').email,
     }
     {
-        name: 'Padme Amidala ',
+        name: 'Padme Amidala',
         role: 'Floor Manager',
         id: '2222222',
         email: 'PadAmi222@homerwork10.com'
         officenumber: 'C12'
         Document.getElementById('2222222').id,
         Document.getElementByName('Padme Amidala').name,
-        Document.getElementByRole('Floor Manager').role&mdash;overridden to return 'Manager',
+        Document.getElementByRole('Floor Manager').role;overridden to return 'Manager',
         Document.getElementByEmail('BenSol000@homework10.com').email,
+    }
+]
+
+Const engineer = [
+    {
+        name: 'Ash Ketchum',
+        role: 'Engineer',
+        id: '1122330',
+        email: 'AshKet112@homerwork10.com'
+        github: 'GottaCatchEmAll'
+        Document.getElementById('1122330').id,
+        Document.getElementByName('Ash Ketchum').name,
+        Document.getElementByRole('Engineer').role;overridden to return 'Engineer',
+        Document.getElementByEmail('AshKet112@homework10.com').email,
+        Document.getElementByGithub('GottaCatchEmAll').github,
+    }
+    {
+        name: 'Tai Kamiya',
+        role: 'Engineer',
+        id: '4455660',
+        email: 'TaiKam445@homerwork10.com'
+        github: 'DigiDestined'
+        Document.getElementById('4455660').id,
+        Document.getElementByName('Tai Kamiya').name,
+        Document.getElementByRole('Engineer').role;overridden to return 'Engineer',
+        Document.getElementByEmail('TaiKam445@homework10.com').email,
+        Document.getElementByGithub('DigiDestined').github,
     }
 ]
